fd0894c70c09c150c80fa298f947d77d8a2d8a00
Marcelo Alves <marceloalves@xipptech.com.br>
2023-03-19 11:32:36 -0300

    Conversão de video/quicktime para video/mp4

--- ftp_download.py
+++ ftp_download.py
@@ -1,6 +1,7 @@
 # Import Module
 import paramiko
 import os
+import ffmpeg
 
 # Fill Required Information
 host = os.getenv("SSH_HOSTNAME", "192.168.0.1")
@@ -26,14 +27,34 @@ def download_file(path_file, location):
     if not os.path.exists(path_dest_full):
         os.makedirs(path_dest_full)
 
+    path_dest_temp_full = path_dest + "/temp/" + location
+    if not os.path.exists(path_dest_temp_full):
+        os.makedirs(path_dest_temp_full)
+
     local_path = path_dest + "/" + location + "/" + filename
+    local_tmp = path_dest + "/temp/" + location + "/" + filename
 
     # Copia o arquivo do servidor para o diretório atual
     ftp_client = ssh_client.open_sftp()
-    ftp_client.get(remote_path, local_path)
+    ftp_client.get(remote_path, local_tmp)
 
     # Fecha a conexão com o servidor SSH e o cliente SFTP
     ftp_client.close()
     ssh_client.close()
 
+    convert_video(local_tmp, local_path)
+
     return local_path
+
+def convert_video(local_tmp, local_path):
+    input_file = local_tmp
+    output_file = local_path
+
+    # Configura o processo de conversão
+    (
+        ffmpeg
+        .input(input_file)
+        .output(output_file, vcodec="h264", acodec="aac", strict="experimental")
+        .overwrite_output()
+        .run()
+    )
